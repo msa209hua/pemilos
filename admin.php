61385f94a3823aa3f36057b720edf0215e31f4cf
Tanszah <artoriapen02@gmail.com>
2023-11-07 15:52:19 +0700

    progres halaman admin

---
+++ admin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    include "koneksi.php";
+    $sql=mysqli_query($conn, "SELECT * FROM db_data_siswa");
+    ?>
+    <table border="1px" collspacing="0" collpadding="15px">
+        <tr>
+            <th>id_pelanggan</th>
+            <th>Nama Pelanggan</th>
+            <th>Alamat</th>
+            <th>No Telepon</th>
+        </tr>
+        <?php foreach ($sql as $row) : ?>
+        <tr align="center">
+                    <td><?= $row["NIS"];?></td>
+                    <td><?= $row["nama"];?></td>
+                    <td><?= $row["kelas"];?></td>
+                    <td>
+                        <?php echo "
+                        <a href='Tanszah_pelangganEdit.php?id=$row[id_pelanggan]'>Edit</a>|"?>
+                        <a href="Tanszah_pelangganHapus.php?id=<?=$row['id_pelanggan'];?>"onclick="return confirm ('Yakin mau hapus?');">Hapus</a></td>
+        </tr>
+        <?php endforeach;?>
+    </table>
+</body>
+</html>
