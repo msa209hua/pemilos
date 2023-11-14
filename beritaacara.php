d879c1db68eaa5ef3c57d6d11fdbd14437aa07ca
Panturas <rafigamer111@gmail.com>
2023-11-14 13:45:36 +0700

    tambahberitaacara

---
+++ beritaacara.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+    <center>
+        <table style="text-align: center; padding-right: 400px;">
+            <tr>
+                <td></td>
+                <td>
+            PEMERINTAH DAERAH PROVINSI JAWA BARAT
+                </td>
+            </tr>
+            <tr>
+            <td></td>
+                <td>
+            DINAS PENDIDIKAN
+                </td>
+            </tr>
+        <tr>
+        <td></td>
+            <td>
+            CABANG DINAS PENDIDIKAN WILAYAH VII
+            </td>
+        </tr>
+        <tr>
+        <td style="text-align: right;"><img src="foto/smk.png" style="padding-left: 370px;" width="20" height="20"></td>
+            <td style="font-weight: 700">
+            SEKOLAH MENENGAH KEJURUAN NEGERI 2 CIMAHI
+            </td>
+        </tr>
+        <tr>
+        <td></td>
+            <td>
+            Jl.Kamarung Km. 1,5 No. 69 Kel. Citeureup Kec. Cimahi  Utara
+            </td>
+        </tr>
+        <tr>
+        <td></td>
+            <td>
+            Telp/Fax:(022) 87805857 Website : http://smkn2cmi.sch.id E-mail : smkn2cmi@yahoo.com
+            </td>
+        </tr>
+        <tr>
+        <td></td>
+            <td>
+            Kota Cimahi - 40512
+            </td>
+        </tr>
+        </table>
+</center>
+</body>
+</html>
