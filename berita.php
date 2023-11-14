c91560e24f688e8afa35bd7586edf54bff911ddf
RdtGames12 <adt.ganteng1234@gmail.com>
2023-11-14 14:13:18 +0700

    kop surat

---
+++ berita.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Bikin Kop Surat</title>
+</head>
+<body>
+<table width="100%">
+<tr>
+<td width="25" align="right"><img src="foto/smk.png" width="40%"></td>
+<td width="50" align="center">PEMERINTAH DAERAH PROVINSI JAWA BARAT<br>DINAS PENDIDIKAN<br>CABANG DINAS PENDIDIKAN WILAYAH VII<br><b>SEKOLAH MENENGAH KEJURUAN NEGERI 2 CIMAHI</b><br><p>Jl.Kamarung Km. 1,5 No. 69 Kel. Citeureup Kec. Cimahi  Utara<br>Telp/Fax:(022) 87805857 Website : http://smkn2cmi.sch.id E-mail : smkn2cmi@yahoo.com <br> Kota Cimahi - 40512</p><br></td>
+<td width="25" align="left"><img src="foto/osis.png" width="60%"></td>
+</tr>
+</table>
+<hr>
+<center>
+    <h3>BERITA ACARA PELAKSANAAN PEMILIHAN KETUA OSIS</h3>
+    <p style="font-size: 18.72px;">Pada hari ini Rabu tanggal 22 bulan November tahun Dua Ribu Dua Puluh Tiga,
+    <br> <ul type="hidden">
+  <li>a. Telah diselenggarakan Pemilihan Ketua Osis Angkatan 16 Tahun Pelajaran 2023/2024 pada </li>
+  <li>Paul Cézanne</li>
+  <li>Claude Monet</li>
+</ul></p>
+</center>
+</body>
+</html>
