8993140577ba21daece91545d3ef773e1f0278e6
Panturas <rafigamer111@gmail.com>
2023-11-14 15:07:33 +0700

    update berita

--- berita.php
+++ berita.php
@@ -6,15 +6,114 @@
 <table width="100%">
 <tr>
 <td width="25" align="right"><img src="foto/smk.png" width="40%"></td>
-<td width="50" align="center">PEMERINTAH DAERAH PROVINSI JAWA BARAT<br>DINAS PENDIDIKAN<br>CABANG DINAS PENDIDIKAN WILAYAH VII<br><b>SEKOLAH MENENGAH KEJURUAN NEGERI 2 CIMAHI</b><br><p>Jl.Kamarung Km. 1,5 No. 69 Kel. Citeureup Kec. Cimahi  Utara<br>Telp/Fax:(022) 87805857 Website : http://smkn2cmi.sch.id E-mail : smkn2cmi@yahoo.com <br> Kota Cimahi - 40512</p><br></td>
+<td width="50" align="center">
+    <b>ORGANISASI SISWA INTRA SEKOLAH (OSIS)
+    <br>SMK NEGERI 2 CIMAHI</b>
+    <br><p>Jl.Kamarung Km. 1,5 No. 69 Kel. Citeureup Kec. Cimahi  Utara
+    <br>Telp/Fax:(022) 87805857 Website : http://smkn2cmi.sch.id E-mail : smkn2cmi@yahoo.com 
+    <br> Kota Cimahi - 40512</p><br></td>
 <td width="25" align="left"><img src="foto/osis.png" width="60%"></td>
 </tr>
 </table>
 <hr>
 <center>
     <h3>BERITA ACARA PELAKSANAAN PEMILIHAN KETUA OSIS</h3>
-    <p style="font-size: 18.72px;">Pada hari ini Rabu tanggal 22 bulan November tahun Dua Ribu Dua Puluh Tiga,
-    <br> 
+    <p>Pada hari ini Rabu tanggal 22 bulan November tahun Dua Ribu Dua Puluh Tiga,</p>
 </center>
+    <table style="padding-left: 345px">
+        <tr>
+            <td>
+                a.
+            </td>
+            <td>
+                Telah Diselenggarakan Pemilihan Ketua Osis Angkatan 16 Tahun Pelajaran 2023/2024
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>pada</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Pukul ..... sampai dengan Pukul .....</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Lokasi</td>
+            <td>: SMK 2 Negeri Cimahi</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Jumlah Pemilihan Seharusnya</td>
+            <td>:</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Jumlah Pemilihan Hadir</td>
+            <td>:</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Jumlah Pemilihan Tidak Hadir</td>
+            <td>:</td>
+        </tr>
+        <tr>
+            <td>b.</td>
+            <td>Hasil Pemilihan :</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>1.Suara Calon 1 (divisi nama calon)</td>
+            <td>:......Suara</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>2.Suara Calon 2 (divisi nama calon)</td>
+            <td>:......Suara</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>3.Suara Calon 3 (divisi nama calon)</td>
+            <td>:......Suara</td>
+        </tr>
+        <tr>
+            <td>c.</td>
+            <td>Catatan Selama Pelaksanaan Pemilihan</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>(diskripsi)</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Berita acara ini dibuat dengan sesungguhnya</td>
+        </tr>
+    </table>
+    </p>
+    <br>
+    <table align="center">
+        <tr>
+            <td>Pembina Osis</td>
+            <td style="color: white;">
+            .......................................................
+        </td>
+            <td></td>
+            <td>Yang membuat berita acara,<br>Ketua Osis</td>
+        </tr>
+        <tr>
+            <td>Tanda Tangan</td>
+            <td>:.......</td>
+            <td></td>
+            <td>Tanda Tangan</td>
+            <td>:.......</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>:.......</td>
+            <td></td>
+            <td>Nama</td>
+            <td>:.......</td>
+        </tr>
+    </table>
 </body>
 </html>
