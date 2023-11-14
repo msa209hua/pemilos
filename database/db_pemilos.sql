ebe27fd73bf56bf56eb8b9bf2cbf14d9197ee50a
bintang <naokikaindra@gmail.com>
2023-11-14 08:10:17 +0700

    edit database

--- database/db_pemilos.sql
+++ database/db_pemilos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Nov 2023 pada 05.27
+-- Waktu pembuatan: 14 Nov 2023 pada 02.08
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 7.4.29
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_pemilos`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_siswa`
+--
+
+CREATE TABLE `data_siswa` (
+  `no` int(11) NOT NULL,
+  `NIS` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `kelas` varchar(25) NOT NULL,
+  `status` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `data_siswa`
+--
+
+INSERT INTO `data_siswa` (`no`, `NIS`, `nama`, `kelas`, `status`) VALUES
+(1, 111111111, 'Figuran A', 'XI RPL A', 'Belum Memilih');
 
 -- --------------------------------------------------------
 
@@ -80,6 +101,12 @@ DELIMITER ;
 --
 
 --
+-- Indeks untuk tabel `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  ADD PRIMARY KEY (`no`);
+
+--
 -- Indeks untuk tabel `log_vote`
 --
 ALTER TABLE `log_vote`
@@ -94,6 +121,12 @@ ALTER TABLE `tb_pemilihan`
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `log_vote`
