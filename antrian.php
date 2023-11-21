59111f3a42de9a339f75a14e937a0c3faf07731f
Tanszah <artoriapen02@gmail.com>
2023-11-21 18:49:38 +0700

    baru cuy

--- antrian.php
+++ antrian.php
@@ -10,26 +10,28 @@ if($status == 'Antri'){
     $antrian = mysqli_query($conn, "SELECT * FROM tb_antrian ORDER BY bilik DESC");
     $total_antrian = mysqli_num_rows($antrian);
 
-    if($total_antrian<3){
+    if($total_antrian<5){
         if($total_antrian==0){
             $bilik = 1;
-        } else {
-            $tot = 0;
-            foreach($antrian as $data){
-                $tot = $tot + $data['bilik'];
+        }
+         else {
+            $arr_bilik=array();
+            foreach($antrian as $k => $data){
+                $arr_bilik[$data['bilik']]=$data['bilik'];
             }
 
             $bilik = 1;
-            if($tot == 0){
-                $bilik = 1;
-            }else if($tot == 1){
-                $bilik = 2;
-            }else if($tot == 3){
-                $bilik = 3;
-            }else if($tot == 4){
-                $bilik = 2;
+            if (!isset($arr_bilik[1])) {
+                $bilik=1;
+            }elseif (!isset($arr_bilik[2])) {
+                $bilik=2;
+            }elseif (!isset($arr_bilik[3])) {
+                $bilik=3;
+            }elseif (!isset($arr_bilik[4])) {
+                $bilik=4;
+            }elseif (!isset($arr_bilik[5])) {
+                $bilik=5;
             }
-            
         }
 
         $sql = mysqli_query($conn, "INSERT INTO tb_antrian VALUES ($nis, $bilik)");
